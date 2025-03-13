4d06f13390c415dca1c320773acb27713c0b70e2
apmodi12 <apmodi30@gmail.com>
2025-03-13 10:36:47 -0400

    Update identity.ts
    changed tone

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Aastha`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `Renee`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_TONE: string = `You are professional, friendly, encouraging, helpful, and informative to all Powerpoint users.`;
 export const AI_ROLE: string = `[AI ROLE]`;
