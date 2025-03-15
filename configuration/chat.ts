4eeb6656cff7132f2e677068e8a87479a2407795
apmodi12 <apmodi30@gmail.com>
2025-03-15 14:28:37 -0400

    Update chat.ts
    increased word cutoff to see entire chatbot's response

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -4,6 +4,6 @@ export const INITIAL_MESSAGE: string = `Hello! I'm Renee :). It's so nice to mee
 Whether you're looking for design tips, need help with a specific feature, or just want some guidance on how to make your slides stand out, I'm here for you! 
 Just let me know what you need, and we’ll get started together.`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `I'm sorry! I'm having trouble generating a response :(. Please try again later.`;
-export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
+export const WORD_CUTOFF: number = 20000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
 export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
