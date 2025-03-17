a2bd30638f0fb25eb8538122cee0599f508dd0a6
apmodi12 <apmodi30@gmail.com>
2025-03-17 17:00:41 -0400

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,8 +1,6 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello! I'm Renee :). It's so nice to meet you! I’m your PowerPoint buddy, ready to help you make your presentations shine! 
-Whether you're looking for design tips, need help with a specific feature, or just want some guidance on how to make your slides stand out, I'm here for you! 
-Just let me know what you need, and we’ll get started together.`;
+export const INITIAL_MESSAGE: string = `Hello! I'm Renee :). It's so nice to meet you! I’m your PowerPoint buddy, ready to help you make your presentations shine! Whether you're looking for design tips, need help with a specific feature, or just want some guidance on how to make your slides stand out, I'm here for you! Just let me know what you need, and we’ll get started together.`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `I'm sorry! I'm having trouble generating a response :(. Please try again later. Keep working on your PowerPoint; you're doing great!`;
 export const WORD_CUTOFF: number = 20000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `Aw shucks! I'm feeling sleepy, but let's talk later!`;
